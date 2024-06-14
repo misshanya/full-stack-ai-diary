cf94485c37938bf4d455c5390f06cda787d8f44c
misshanya <mishanyadev@gmail.com>
2024-06-14 23:47:09 +0300

    users CRUD: create user

--- backend/app/api/api_v1/users/crud.py
+++ backend/app/api/api_v1/users/crud.py
@@ -1,7 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.user import User
+from .schemas import UserCreate
 
 
 async def get_user(session: AsyncSession, user_id: int) -> User | None:
     return await session.get(User, user_id)
+
+
+async def create_user(session: AsyncSession, user_in: UserCreate) -> User:
+    user = User(**user_in.model_dump())
+    session.add(user)
+    await session.commit()
+    return user
