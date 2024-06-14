8b428150b779dd804a9e5821155034a5752cd389
misshanya <mishanyadev@gmail.com>
2024-06-14 16:59:26 +0300

    add host and port from settings to uvicorn run

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,5 +12,7 @@ app.include_router(
 if __name__ == '__main__':
     uvicorn.run(
         'main:app',
+        host=settings.run.host,
+        port=settings.run.port,
         reload=True,
     )
