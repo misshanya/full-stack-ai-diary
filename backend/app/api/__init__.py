5bd665cecfc17229a9c7fb71a375d12cd513b69d
misshanya <mishanyadev@gmail.com>
2024-06-14 17:45:21 +0300

    add api v1 prefix

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from .api_v1 import router as router_api_v1
+from core.config import settings
 
 router = APIRouter(
 
 )
 router.include_router(
     router_api_v1,
+    prefix=settings.api.v1.prefix,
 )
--- backend/app/api/api_v1/__init__.py
+++ backend/app/api/api_v1/__init__.py
@@ -5,7 +5,7 @@ from .users import router as users_router
 from core.config import settings
 
 router = APIRouter(
-
+    prefix=settings.api.v1.prefix,
 )
 router.include_router(
     users_router,
