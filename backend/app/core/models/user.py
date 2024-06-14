fc812058995ee1b9efe10d2da1a7abab1dca2543
misshanya <mishanyadev@gmail.com>
2024-06-14 16:46:15 +0300

    add user SQLAlchemy model

---
+++ backend/app/core/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import UniqueConstraint
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
+
+    first_name: Mapped[str] = mapped_column(String(20))
+    last_name: Mapped[str] = mapped_column(String(20))
+    sch_class: Mapped[int] = mapped_column()
