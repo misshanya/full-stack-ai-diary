2da1d6a0dbc00cf6b0ddcee9cc07d7242f12c4fc
misshanya <mishanyadev@gmail.com>
2024-06-15 12:44:36 +0300

    create schemas for user update

--- backend/app/api/api_v1/users/schemas.py
+++ backend/app/api/api_v1/users/schemas.py
@@ -14,6 +14,19 @@ class UserCreate(UserBase):
     pass
 
 
+class UserUpdate(UserCreate):
+    pass
+
+
+class UserUpdatePartial(UserCreate):
+    username: str | None = None
+    password: str | None = None
+
+    first_name: str | None = None
+    last_name: str | None = None
+    sch_class: int | None = None
+
+
 class User(UserBase):
     model_config = ConfigDict(from_attributes=True)
 
