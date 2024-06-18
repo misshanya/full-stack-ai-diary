0d8a2543250c5fd995829b9cda909f206e2ef9dc
misshanya <mishanyadev@gmail.com>
2024-06-18 18:27:42 +0300

    login views

--- backend/app/api/api_v1/auth/views.py
+++ backend/app/api/api_v1/auth/views.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from core.config import settings
+from core.models import User
+
+from .validation import validate_auth_user
+
 
 router = APIRouter(
     tags=['Auth'],
     prefix=settings.api.v1.auth,
 )
+
+
+@router.post('/test-login/')
+async def test_login(user: User = Depends(validate_auth_user)):
+    return user
+
+
+@router.post('/login/')
+async def login(
+    user: User = Depends(validate_auth_user),
+):
+    pass
