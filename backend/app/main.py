a538d67d2b7fe5ab84b0c10cefd4e8f24578c9bb
misshanya <mishanyadev@gmail.com>
2024-06-18 18:27:18 +0300

    add cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,28 @@ import uvicorn
 
 from api import router as api_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
 
 app = FastAPI()
 app.include_router(
     api_router,
+)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8001",
+    "http://127.0.0.1:8001"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
